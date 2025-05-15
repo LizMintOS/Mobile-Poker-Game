2a8387b57ce32b2ac826f228839ebdbf247aa690
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-15 17:19:08 +1200

    chore: create firebase service

---
+++ src/services/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import {
+  CACHE_SIZE_UNLIMITED,
+  initializeFirestore,
+  persistentLocalCache,
+  persistentSingleTabManager,
+  connectFirestoreEmulator,
+} from "firebase/firestore";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCP2yjcvFxkIcI-Pns55KLyZO_8MVESSv0",
+  authDomain: "poker-game-2025.firebaseapp.com",
+  projectId: "poker-game-2025",
+  storageBucket: "poker-game-2025.firebasestorage.app",
+  messagingSenderId: "74684296066",
+  appId: "1:74684296066:web:64d4dcb1549a702ad2fd9b"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentSingleTabManager({}),
+    cacheSizeBytes: CACHE_SIZE_UNLIMITED,
+  }),
+});
+export const auth = getAuth(app);
+
+// connectFirestoreEmulator(db, '127.0.0.1', 8080);
+// connectAuthEmulator(auth, "http://localhost:9099");
