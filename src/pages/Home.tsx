59a46ea45d27b9b5a20c0b474bc5ab649412819c
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-20 16:26:14 +1200

    feat: create home page and implement react router

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const HomePage = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/icon.png" className="logo" alt="liz-poker-game logo" />
+        </a>
+      </div>
+      <h1>poker-game</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
