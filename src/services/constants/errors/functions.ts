7aa3d6e65d0050f1b9273b091d28c5a59c6fc608
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-22 15:24:46 +1200

    refactor: simplify map error function

--- src/services/constants/errors/functions.ts
+++ src/services/constants/errors/functions.ts
@@ -1,93 +1,44 @@
 import { FirestoreError } from "firebase/firestore";
 import { AuthError } from "firebase/auth";
-import { ErrorMessage, LocalError } from "./types";
+import { Errors } from "./types";
 
 export const mapFirestoreErrorToAuthError = (
-    error: FirestoreError | AuthError | any
-  ): LocalError => {
-    switch (error.code) {
-      case "not-found":
-        return ErrorMessage.RESOURCE_NOT_FOUND
-      case "auth/email-already-in-use":
-        return {
-          message: ErrorMessage.EMAIL_EXISTS,
-          code: "EMAIL_EXISTS",
-        };
-      case "auth/user-not-found":
-        return {
-          message: ErrorMessage.USER_NOT_FOUND,
-          code: "USER_NOT_FOUND",
-        };
-      case "auth/user-not-logged-in":
-        return {
-          message: ErrorMessage.USER_NOT_LOGGED_IN,
-          code: "USER_NOT_LOGGED_IN",
-        };
-      case "auth/invalid-credential":
-        return {
-          message: ErrorMessage.INVALID_CREDENTIALS,
-          code: "INVALID_CREDENTIALS",
-        };
-      case "auth/invalid-email":
-        return {
-          message: ErrorMessage.INVALID_EMAIL,
-          code: "INVALID_EMAIL",
-        };
-      case "auth/invalid-password":
-        return {
-          message: ErrorMessage.INVALID_PASSWORD,
-          code: "INVALID_PASSWORD",
-        };
-      case "auth/invalid-user-token":
-        return {
-          message: ErrorMessage.INVALID_DETAILS,
-          code: "INVALID_DETAILS",
-        };
-      case "auth/network-request-failed":
-        return {
-          message: ErrorMessage.NETWORK_ERROR,
-          code: "NETWORK_ERROR",
-        };
-      case "invalid-argument":
-        return {
-          message: ErrorMessage.INVALID_DETAILS,
-          code: "INVALID_DETAILS",
-        };
-      case "already-exists":
-        return {
-          message: ErrorMessage.RESOURCE_EXISTS,
-          code: "RESOURCE_EXISTS",
-        };
-      case "unauthenticated":
-        return {
-          message: ErrorMessage.RESOURCE_UNAUTH,
-          code: "RESOURCE_UNAUTH",
-        };
-      case "permission-denied":
-        return {
-          message: ErrorMessage.ACCESS_DENIED,
-          code: "ACCESS_DENIED",
-        };
-      case "aborted":
-        return {
-          message: ErrorMessage.FIREBASE_ERROR,
-          code: "FIREBASE_ERROR",
-        };
-      case "internal":
-        return {
-          message: ErrorMessage.API_ERROR,
-          code: "API_ERROR",
-        };
-      case "unimplemented":
-        return {
-          message: ErrorMessage.MISSING_FUNCTION,
-          code: "MISSING_FUNCTION",
-        };
-      default:
-        return {
-          message: ErrorMessage.UNKNOWN_ERROR,
-          code: error.code,
-        };
-    }
-  };
-  
+  error: FirestoreError | AuthError | any
+): string => {
+  switch (error.code) {
+    case "not-found":
+      return Errors.RESOURCE_NOT_FOUND;
+    case "auth/email-already-in-use":
+      return Errors.EMAIL_EXISTS;
+    case "auth/user-not-found":
+      return Errors.USER_NOT_FOUND;
+    case "auth/user-not-logged-in":
+      return Errors.USER_NOT_LOGGED_IN;
+    case "auth/invalid-credential":
+      return Errors.INVALID_CREDENTIALS;
+    case "auth/invalid-email":
+      return Errors.INVALID_EMAIL;
+    case "auth/invalid-password":
+      return Errors.INVALID_PASSWORD;
+    case "auth/invalid-user-token":
+      return Errors.INVALID_DETAILS;
+    case "auth/network-request-failed":
+      return Errors.NETWORK_ERROR;
+    case "invalid-argument":
+      return Errors.INVALID_DETAILS;
+    case "already-exists":
+      return Errors.RESOURCE_EXISTS;
+    case "unauthenticated":
+      return Errors.RESOURCE_UNAUTH;
+    case "permission-denied":
+      return Errors.ACCESS_DENIED;
+    case "aborted":
+      return Errors.FIREBASE_ERROR;
+    case "internal":
+      return Errors.API_ERROR;
+    case "unimplemented":
+      return Errors.MISSING_FUNCTION;
+    default:
+      return Errors.UNKNOWN_ERROR;
+  }
+};
