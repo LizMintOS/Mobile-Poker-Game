70b35a9cec6195ea3dbec3fddcacdfda4986a660
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-22 14:49:38 +1200

    chore: remove status wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
 import { ServiceWorkerManager } from "./services/serviceWorkerManager.tsx";
 import { AuthProvider } from "./contexts/AuthProvider.tsx";
-import { StatusWrapper } from "./components/common/StatusWrapper.tsx";
 import { StatusProvider } from "./contexts/StatusProvider.tsx";
 // import { NavigationProvider } from "./contexts/NavProvider.tsx";
 
@@ -18,7 +17,6 @@ root.render(
       <AuthProvider>
         {/* <NavigationProvider> */}
           <ServiceWorkerManager />
-          <StatusWrapper />
           <App />
         {/* </NavigationProvider> */}
       </AuthProvider>
