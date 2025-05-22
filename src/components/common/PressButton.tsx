0952fc5e387ae73fce65f5a246402390265e507d
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-23 11:13:08 +1200

    feat: add PressButton component for reusable button functionality

---
+++ src/components/common/PressButton.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+interface PressButtonProps {
+  type: "button" | "submit";
+  disabled?: boolean;
+  onClick?: () => void;
+  style?: string;
+  children: ReactNode;
+}
+
+const PressButton = ({
+  type = "button",
+  disabled = false,
+  onClick,
+  style = "",
+  children,
+}: PressButtonProps) => {
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+      className={`rounded-xl py-2 px-4 text-white text-lg border-b-8 transition-border duration-100 font-bold cursor-pointer border-x-2 hover:border-b-4 overflow-hidden ${style}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default PressButton;
