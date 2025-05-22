3b7e077dcc193e9a82e75e67e1f1b9811df5c772
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-23 10:57:50 +1200

    feat: create header (base code from checkpoint)

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,35 @@
+import { useAuth } from "../../contexts/AuthProvider";
+import { useAuthActions } from "../../api/auth/functions";
+import { useParams } from "react-router";
+
+const Header = () => {
+  const { logoutUser } = useAuthActions();
+  const { username } = useParams();
+
+  const handleLogout = async () => {
+    await logoutUser();
+  };
+
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="left-content">
+          <h3 className="username">{username}</h3>
+        </div>
+        <div className="button-group">
+          <a href="/" className="nav-link">
+            Create Game
+          </a>
+          <a href="/groups/public" className="nav-link">
+            Games
+          </a>
+          <button className="bg-red-400 text-white border-none" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
