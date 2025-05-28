bdfb61d3ea42c9da0062adadaa75e825d6e2d6a1
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-05-28 17:56:15 +1200

    chore: init new form for create game

--- src/pages/CreateGame.tsx
+++ src/pages/CreateGame.tsx
@@ -1,10 +1,11 @@
+import { CreateGameForm } from "../components/CreateGameForm";
 
 const CreateGamePage = () => {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <div className="bg-white shadow-xl rounded-2xl p-8 w-full text-center border-1 border-slate-100/50">
-          <p>games</p>
+      <div className="flex justify-center items-center align-center relative min-h-4/5">
+        <div className="bg-white shadow-xl rounded-2xl p-8 w-fit text-center border-1 border-slate-100/50">
+          <CreateGameForm />
         </div>
       </div>
     </>
