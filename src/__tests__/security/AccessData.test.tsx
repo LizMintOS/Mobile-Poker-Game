153f5885fe5f8e4ff84fa1ea0a19456eab7565a0
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-06-20 18:29:25 +1200

    feat: refactor AccessData tests to improve structure and add additional security rule tests

--- src/__tests__/security/AccessData.test.tsx
+++ src/__tests__/security/AccessData.test.tsx
@@ -32,9 +32,11 @@ afterAll(async () => {
   await testEnvironment.cleanup();
 });
 
-test("Auth user can create game", async () => {
+describe("Firestore security rules", () => {
   const gameId = "game1";
   const creatorUid = "user123";
+  const otherUid = "otherUser";
+  const playerId = "player123";
 
   const mockGame: Game = {
     id: gameId,
@@ -46,9 +48,23 @@ test("Auth user can create game", async () => {
     turnOrder: [],
     scores: [],
   };
-  const db = testEnvironment.authenticatedContext(creatorUid).firestore();
 
-  await assertSucceeds(
-    fs.setDoc(fs.doc(db, `games/${gameId}/`), mockGame)
-  );
+  function getDbForUser(uid: string) {
+    return testEnvironment.authenticatedContext(uid).firestore();
+  }
+
+  function getDbUnauthenticated() {
+    return testEnvironment.unauthenticatedContext().firestore();
+  }
+
+  test("Auth user can create game", async () => {
+    const db = getDbForUser(creatorUid);
+
+    await assertSucceeds(fs.setDoc(fs.doc(db, `games/${gameId}/`), mockGame));
+  });
+
+  test("Auth user can read a game", async () => {
+    const db = getDbForUser(otherUid);
+    await assertSucceeds(fs.getDoc(fs.doc(db, `games/${gameId}`)));
+  });
 });
