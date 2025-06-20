655e0cf8a46cdb3930d6195b2747cc9064c93825
Elizabeth Minty (1000120989) <MINTEP1@student.op.ac.nz>
2025-06-20 17:03:33 +1200

    feat: create EndGameInfoBox component for displaying final scores and winners

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -5,7 +5,7 @@ import { LoadingWrapper } from "../components/common/LoadingWrapper";
 import { useGame } from "../contexts/GameProvider";
 import { usePlayerProxy } from "src/api/proxies/PlayerProxy";
 
-import { Player, PlayerScore } from "../api/types";
+import { Player } from "../api/types";
 
 import PlayingCardList from "../components/cards/CardList";
 import PressButton from "../components/common/buttons/PressButton";
@@ -15,6 +15,7 @@ import {
   removeCardsFromDeck,
   scoreHand,
 } from "../utils/cards";
+import EndGameInfoBox from "src/components/EndGameInfoBox";
 
 const Game = () => {
   const { currentUser } = useAuth();
@@ -119,13 +120,6 @@ const Game = () => {
     setLoading(false);
   };
 
-  const getWinners = (scores: PlayerScore[]) => {
-    const maxScore = Math.max(...scores.map((score) => score.score));
-    return scores
-      .map((score, idx) => (score.score === maxScore ? idx : -1))
-      .filter((idx) => idx !== -1);
-  };
-
   return (
     <>
       <div className="flex justify-center items-center">
@@ -204,53 +198,7 @@ const Game = () => {
             </>
           ) : (
             <div className="justify-center flex w-full">
-              {game && (
-                <div className="bg-green-100 shadow-md shadow-green-900 rounded-2xl p-8 self-center w-fit text-center border-1 border-slate-100/50">
-                  <h1 className="text-3xl font-bold text-center mb-8 underline underline-offset-6">
-                    End of Game
-                  </h1>
-                  <div className="space-y-2 mb-6 pl-4">
-                    <p className="text-left text-xl font-semibold mb-4 underline underline-offset-4">
-                      Final Scores
-                    </p>
-                    <ul className="pl-2">
-                      {game.scores.map((hand, idx) => (
-                        <li
-                          key={idx}
-                          className="text-base italic text-left mb-4"
-                        >
-                          <span className="font-medium pr-4">
-                            Player {idx + 1}:
-                          </span>{" "}
-                          {hand.name}
-                          <span className="font-bold pl-4">
-                            {hand.score} pts
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-
-                  <div className="space-y-2 mb-6 pl-4">
-                    <div className="text-left text-xl font-semibold flex flex-row">
-                      <p className="underline underline-offset-4 pr-4 mb-1">
-                        Winner{getWinners(game.scores).length > 1 ? "s" : ""}:{" "}
-                      </p>
-                      <div className="items-center flex">
-                        {getWinners(game.scores).map((winnerIdx) => (
-                          <span className="text-lg pr-2 ">
-                            Player {winnerIdx + 1}
-                            {game.scores.length > 1 &&
-                            winnerIdx !== game.scores.length - 1
-                              ? ", "
-                              : ""}
-                          </span>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
+              {game && <EndGameInfoBox game={game} />}
             </div>
           )}
         </div>
